1dd0630421d87e1032b43c3d907615b9f7065344
VikasSivashankaran <vikasbmca@gmail.com>
2024-09-09 17:20:59 +0530

    new:

---
+++ pandas/pandasDF.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data)
+print(df)
+result = df.loc[1]
+print(result)
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+print(df)
+print(df.loc['day2'])
+# print(df.loc[0])
+
+pd.options.display.max_rows = 9999
+#it prints only 1st 5 and last 5 rows
+df = pd.read_csv('data.csv')
+print("csv data \n", df)
+
+# #to print all rows
+# print("csv data \n", df.to_string())
+
+df = pd.read_csv('data.csv')
+#analyze data
+print(df.head(10))
+print(df.tail(11))
+print("before ",df.info())
+
+print(df.duplicated())
+df.drop_duplicates(inplace = True)
+print("after removing duplicate \n",df.to_string())
+print("after ",df.info())
