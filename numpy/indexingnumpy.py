24661ef4c7a31f98a04180e741e1b43dac38f824
VikasSivashankaran <vikasbmca@gmail.com>
2025-01-27 09:40:47 +0530

    new

--- numpy/indexingnumpy.py
+++ numpy/indexingnumpy.py
@@ -6,7 +6,7 @@ import numpy as np
 arr = np.array([[-1, 2, 0, 4],
                 [4, -0.5, 6, 0],
                 [2.6, 0, 7, 8],
-                [3, -7, 4, 2.0]])
+                [3, -7, 4, 2.0]],dtype='int')
 
 print("array",arr[1,3])
 # Slicing array
